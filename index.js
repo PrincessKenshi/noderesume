2b02c3e5d055055b0fb083cdddd89751c42f6bfe
LAPTOP-HUARQIE4\kenshi <princesskenshi73@gmail.com>
2024-04-30 12:40:50 +0800

    d

--- index.js
+++ index.js
@@ -4,45 +4,68 @@ const app = express();
 const port = 3000;
 
 const profile = {
-    personalInfo:  
-        {
-        name: 'Princess Kenshi P. Quitor',
-        birthdate: 'October 9, 2003',
-        gender: 'Female',
-        age: '20',
-        address: 'Sitio San Vicente, Lahug, Cebu City',
-        phoneNumber: '0927-515-0359',
-        email: 'princesskenshi73@gmail.com'
-    },
-    aboutme: {
-        description: "Hello, I am Princess Kenshi P. Quitor, but you can call me Kenshi. I'm a digital artist and 2nd year IT college student. I'm currently learning coding and exploring more things about programming. A hardworking person who wants to be a full-stack developer soon, in order to create new and more creative ideas."
-    },
-    skills: [
-        'Illustrator',
-        'HTML/CSS',
-        'Javascript'
+    personalInfo: [
+            {
+                name: "Princess Kenshi P. Quitor",
+                Address: "Sitio San Vicente, Lahug, Cebu City",
+                ContactNo: "0927-515-0359",
+                Email: "princesskenshi73@gmail.com",
+                Birthdate: "October 9, 2003",
+                Gender: "Female",
+                Age: "20",
+                profile_pic: "https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/336648301_873752613730450_320881333241351639_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHOtyirn024oKdxXd-_hmloPQJxbH9g4sM9AnFsf2Diw1xrhZHadgdRGf091QrdmqUTa3S8aChHsAgKktRqdI-7&_nc_ohc=BP4s9RXEejUAb7GKmUa&_nc_ht=scontent-mnl1-1.xx&oh=00_AfCWIfdumqzGLjjMHc6xJqtBb6emXQGY1yIf0rhBycB19g&oe=66298123"
+            }
     ],
-  education:
-    {
-    school: 'University of Southern Philippines Foundation',
-    level: 'Bachelor of Science in Information Technology (BSIT)',
-    year: '2022-2026',
-    address: 'Salinas Drive, Lahug, Cebu City',
-  },
-education: 
-{
-    school: 'University of Southern Philippines Foundation',
-    level: 'Senior High School [STEM]',
-    year: '2020-2022',
-    address: 'Salinas Drive, Lahug, Cebu City'
-},
-  experience: [
-    'No Experience Yet'
-  ],
-  reference: [
-    'No Reference Yet'
-  ],
+    
+    About_Me: [
+            {
+                Description: "Hello, I am Princess Kenshi P. Quitor, but you can call me Kenshi. I'm a digital artist and 2nd year IT college student. I'm currently learning coding and exploring more things about programming. A hardworking person who wants to be a full-stack developer soon, in order to create new and more creative ideas."
+            }
+    ],
+    
+        Skills: [
+            {
+                SkillName: "Illustrator",
+                Percentage: "85%"
+            },
+            {
+                SkillName: "HTML/CSS",
+                Percentage: "30%"
+            },
+            {
+                SkillName: "Javascript",
+                Percentage: "15%"
+            }
+        ],
+    
+        Education: [
+            {
+                school: 'University of Southern Philippines Foundation',
+                level: 'Bachelor of Science in Information Technology (BSIT)',
+                year: '2022-2026',
+                address: 'Salinas Drive, Lahug, Cebu City',
+            },
+            {
+                school: 'University of Southern Philippines Foundation',
+                level: 'Senior High School [STEM]',
+                year: '2020-2022',
+                address: 'Salinas Drive, Lahug, Cebu City'
+            }
+        ],
+    
+        Work_Experience: [
+            {
+                Description: "No Experience Yet"
+            }
+        ],
+    
+        References: [
+            {
+                Description: "No Reference Yet"
+            }
+        ],
 };
+    
 
 // Enable CORS for all routes
 app.use(cors());
